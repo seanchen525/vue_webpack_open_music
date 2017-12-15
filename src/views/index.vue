487dd98175776773a82c357e29995b64ad65d7ab
seanOfficeUbuntu <chenxiaoyong525@hotmail.com>
2017-12-15 17:52:06 -0500

    init project

---
+++ src/views/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        这是{{page}}页面
+    </div>
+</template>
+<script>
+import "../style/style.css"
+
+export default {
+    data: function () {
+        return {
+            page: 'index'
+        }
+    }
+}
+</script>
